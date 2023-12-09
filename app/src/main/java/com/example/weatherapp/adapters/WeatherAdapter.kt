b9baadf5c56941b157777bcd1585166823f6497d
Ib1za94 <kolibelrazuma@gmail.com>
2023-12-09 20:46:52 +0200

    Create Comparator

--- app/src/main/java/com/example/weatherapp/adapters/WeatherAdapter.kt
+++ app/src/main/java/com/example/weatherapp/adapters/WeatherAdapter.kt
@@ -1,11 +1,12 @@
 package com.example.weatherapp.adapters
 
 import android.view.View
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherapp.databinding.ListItemBinding
 
-class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
+class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>(Comparator()) {
 
     class Holder(view: View): RecyclerView.ViewHolder(view){
         val binding = ListItemBinding.bind(view)
@@ -15,5 +16,15 @@ class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
             tvCondition.text = item.condition
             tvTemp.text = item.currentTemp
         }
+    }
+
+    class Comparator : DiffUtil.ItemCallback<WeatherModel>(){
+        override fun areItemsTheSame(oldItem: WeatherModel, newItem: WeatherModel): Boolean {
+            return oldItem == newItem
+        }
+        override fun areContentsTheSame(oldItem: WeatherModel, newItem: WeatherModel): Boolean {
+            return oldItem == newItem
+        }
+
     }
 }
