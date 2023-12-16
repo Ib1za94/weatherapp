d27b87c53f4d45973990286a19a8dc251e4fd913
Ib1za94 <kolibelrazuma@gmail.com>
2023-12-16 21:02:59 +0200

    Take data from API

--- app/src/main/java/com/example/weatherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragments/MainFragment.kt
@@ -2,6 +2,7 @@ package com.example.weatherapp.fragments
 
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,10 +11,14 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.FragmentActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.weatherapp.R
 import com.example.weatherapp.adapters.VpAdapter
 import com.example.weatherapp.databinding.FragmentMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
+const val API_KEY = "f6461ef1d1c544769fc175736230911"
 
 class MainFragment : Fragment() {
     private val flist = listOf(
@@ -39,6 +44,7 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
         init()
+        requestWeatherData("Kyiv")
     }
 
     private fun init() = with(binding){
@@ -61,6 +67,28 @@ class MainFragment : Fragment() {
             permissionListener()
             pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
+    }
+
+    private fun requestWeatherData(city: String){
+    val url = "https://api.weatherapi.com/v1/forecast.json?key=" +
+            API_KEY +
+            "&q=" +
+            city +
+            "&days=" +
+            "3" +
+            "&aqi=no&alerts=no\n"
+        val queue = Volley.newRequestQueue(context)
+        val request = StringRequest(
+            Request.Method.GET,
+            url,
+            {
+                result -> Log.d("RequestSuccess", "Result: $result")
+            },
+            {
+                error -> Log.d("RequestError", "Error: $error")
+            }
+        )
+        queue.add(request)
     }
 
     companion object {
